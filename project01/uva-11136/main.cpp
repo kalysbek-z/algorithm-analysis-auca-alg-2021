d7a0fe68ccb9eb4c13f3410f9ab2658e828240b9
kalysbek-z <kalysbek_z@auca.kg>
2021-04-05 01:11:01 +0600

    11136 accepted

---
+++ project01/uva-11136/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n && n != 0)
+    {
+        multiset<int> bills;
+        long sum = 0;
+        while (n--)
+        {
+            int m;
+            cin >> m;
+
+            while (m--)
+            {
+                int k;
+                cin >> k;
+                bills.insert(k);
+            }
+
+            auto max = --bills.end();
+            auto min = bills.begin();
+
+            sum += *max - *min;
+
+            bills.erase(min);
+            bills.erase(max);
+        }
+
+        // cout << *max << "," << *min << "\n";
+        cout << sum << "\n";
+    }
+}
