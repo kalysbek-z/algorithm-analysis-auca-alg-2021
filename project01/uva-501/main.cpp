18e6e1d52f2033083f19867ae1912805611385b0
kalysbek-z <kalysbek_z@auca.kg>
2021-04-10 15:15:44 +0600

    501 presentationE

---
+++ project01/uva-501/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    scanf("%d", &k);
+    int c = 0;
+    while (k--)
+    {
+        if (c > 0)
+        {
+            printf("\n");
+        }
+        int m, n;
+        scanf("%d %d", &m, &n);
+        vector<int> addElem;
+        vector<int> queueOfElem;
+
+        multiset<int> elem;
+
+        // initializing elements
+        for (int i = 0; i < m; i++)
+        {
+            int num;
+            scanf("%d", &num);
+            addElem.push_back(num);
+        }
+        //initializing queue of adding
+        for (int i = 0; i < n; i++)
+        {
+            int num;
+            scanf("%d", &num);
+            queueOfElem.push_back(num);
+        }
+
+        elem.insert(addElem[0]);
+        auto iter = elem.begin(); //current elem[index] number (answer)
+
+        int index = 0;
+
+        for (int i = 1; i <= m; i++)
+        {
+            while (index < n && queueOfElem[index] == i)
+            {
+                printf("%d\n", *iter);
+                index++;
+                iter++;
+            }
+
+            elem.insert(addElem[i]);
+
+            if (addElem[i] < *iter || iter == elem.end())
+            {
+                iter--;
+            }
+        }
+        c++;
+    }
+}
