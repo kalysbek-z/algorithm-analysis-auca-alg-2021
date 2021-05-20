6466bf6d37e794b3cca33352c25766b43e7954f5
kalysbek-z <kalysbek_z@auca.kg>
2021-05-20 22:45:14 +0600

    729 presentE

---
+++ project-2/uva-729/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void hammingDistance(int n, int h, string s)
+{
+    if (n == s.size())
+    {
+        if (h == 0)
+        {
+            cout << s << "\n";
+        }
+        return;
+    }
+    hammingDistance(n, h, s + '0');
+    hammingDistance(n, h - 1, s + '1');
+}
+
+int main()
+{
+    int d;
+    cin >> d;
+    // cin.ignore();
+    // cin.ignore();
+    int n, h;
+    while (d--)
+    {
+        cin >> n >> h;
+        string s = "";
+
+        hammingDistance(n, h, s);
+    }
+    // cout << "\n";
+}
