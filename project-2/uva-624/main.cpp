662c4b1f8ab6b7f4be96daf9b49043c5e226b6c0
kalysbek-z <kalysbek_z@auca.kg>
2021-05-21 23:37:26 +0600

    624 wrong

---
+++ project-2/uva-624/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int d, n;
+int maxSum = 0;
+vector<int> vec;
+
+void solve(int cur, int sum, const vector<int> &v, vector<int> &t)
+{
+    if ((int)v.size() == cur)
+    {
+        if (sum > maxSum && sum <= d)
+        {
+            maxSum = sum;
+            vec = t;
+        }
+    }
+    else
+    {
+        t[cur] = 0;
+        solve(cur + 1, sum, v, t);
+        t[cur] = 1;
+        solve(cur + 1, sum + v[cur], v, t);
+    }
+    // if (cur == (int)v.size())
+    // {
+    //     cout << "sum: " << sum << "\n";
+    // }
+    // else
+    // {
+    //     solve(cur + 1, sum, v);
+    //     solve(cur + 1, sum + v[cur], v);
+    // }
+}
+
+int main()
+{
+    for (; cin >> d >> n;)
+    {
+        vector<int> tracks(n);
+        vector<int> list(n);
+
+        for (auto &e : tracks)
+        {
+            cin >> e;
+        }
+
+        solve(0, 0, tracks, list);
+
+        for (int i = 0; i < vec.size(); i++)
+        {
+            if (vec[i] == 1)
+            {
+                cout << tracks[i] << " ";
+            }
+        }
+        cout << "sum:" << maxSum << "\n";
+    }
+}
