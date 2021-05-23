0f73cfecd7fba3afc81e8af24a9ae691e740fbab
kalysbek-z <kalysbek_z@auca.kg>
2021-05-23 19:20:30 +0600

    11518 runtime error

---
+++ project-2/uva-11518/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int total;
+
+enum class Status
+{
+    notVisited,
+    inProgress,
+    visited
+};
+
+void dfs(int vertice, vector<vector<int>> &graph, vector<Status> &vertices)
+{
+    total++;
+    vertices[vertice] = Status::inProgress;
+    for (auto &e : graph[vertice])
+    {
+        if (vertices[e] == Status::notVisited)
+        {
+            dfs(e, graph, vertices);
+        }
+    }
+
+    vertices[vertice] = Status::visited;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m, l;
+        cin >> n >> m >> l;
+
+        vector<vector<int>> graph(1000);
+        vector<Status> states(1000, Status::notVisited);
+        total = 0;
+
+        while (m--)
+        {
+            int u, v;
+            cin >> u >> v;
+
+            graph[u].push_back(v);
+        }
+        while (l--)
+        {
+            int h;
+            cin >> h;
+            if (states[h] == Status::notVisited)
+            {
+                dfs(h, graph, states);
+            }
+        }
+
+        cout << total << "\n";
+    }
+}
